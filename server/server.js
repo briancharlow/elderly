3d6554df2c3d98dbe9213203e5cde6b1daf98c0d
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-11-21 13:47:46 +0300

    sets up redis

--- server/server.js
+++ server/server.js
@@ -8,18 +8,35 @@ const sql = require('mssql');
 const session = require('express-session')
 const { v4 } = require('uuid');
 const { clientRoute } = require('./Routes/clientRoutes');
-
+const RedisStore = require("connect-redis").default;
+const { createClient } = require("redis");
 
 app.use(express.json());
 async function startApp() {
+
+
+
+
     const port = process.env.PORT;
     const oneDay = 1000 * 60 * 60 * 24
     const pool = await sql.connect(dbConfig);
+
+    const redisClient = createClient();
+    redisClient.connect();
+    console.log('connected to redis');
+
+    const redisStore = new RedisStore({
+        client: redisClient,
+        prefix: ''
+
+    })
+
     app.use((req, res, next) => {
         req.pool = pool;
         next();
     })
     app.use(session({
+        store: redisStore,
         secret: process.env.SECRET,
         resave: true,
         saveUninitialized: false,
@@ -36,6 +53,18 @@ async function startApp() {
     app.use(Route);
     app.use(caregiverRoute);
     app.use(clientRoute);
+
+    app.use("*", (req, res, next) => {
+        const error = new Error("Route Not found");
+        next({
+            status: 404,
+            message: error.message,
+        });
+
+    });
+    app.use((error, req, res, next) => {
+        res.status(error.status).json(error.message);
+    });
 
 
 
