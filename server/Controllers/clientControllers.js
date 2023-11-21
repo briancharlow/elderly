d284e6c93bdf47bcc3a1caf5b4039300ccc2e753
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-11-21 13:00:21 +0300

    makes modifications to the search controller

--- server/Controllers/clientControllers.js
+++ server/Controllers/clientControllers.js
@@ -4,13 +4,25 @@ async function searchCaregiver(req, res) {
 
     try {
         if (pool.connected) {
-            let results = await pool.request().input('searchTerm', searchTerm).execute('SearchCaregiver');
+            let results = await pool.request()
+                .input('searchTerm', searchTerm)
+                .execute('SearchCaregiver');
+
+            console.log(results)
             const caregivers = results.recordsets[0];
-            res.status(200).json({
-                success: true,
-                message: "Caregivers found",
-                caregivers: caregivers,
-            })
+            if (results.rowsAffected[0] === 0) {
+                res.status(404).json({
+                    success: false,
+                    message: "No caregiver found",
+                })
+            } else {
+                res.status(200).json({
+                    success: true,
+                    message: "Caregivers found",
+                    caregivers: caregivers,
+                })
+            }
+
         }
 
     } catch (error) {
