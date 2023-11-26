ccb30e3ed19d5feb6974022b8a5ea9b0389ac057
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2023-11-26 17:01:07 +0300

    changes session authorization

--- server/middleware/sessionAuthorization.js
+++ server/middleware/sessionAuthorization.js
@@ -6,14 +6,14 @@ const sessionAuthorization = (req, res, next) => {
     const refresh_token = req.cookies.refreshtoken;
     const access_token = req.cookies.accesstoken;
     // check if there are no tokens
-    if (!token && !refresh_token) {
-        return res.status(404).json({
+    if (!access_token && !refresh_token) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
             message: "Access denied.No tokens",
         });
     }
     try {
         // check if access token is valid
-        const decoded = jwt.verify(token, process.env.SECRET);
+        const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET);
         req.user = decoded;
         next();
     } catch (error) {
@@ -25,20 +25,20 @@ const sessionAuthorization = (req, res, next) => {
                     process.env.REFRESH_TOKEN_SECRET
                 );
                 // create new access token
-                const token = jwt.sign(
-                    { username: decoded.fullname, roles: decoded.roles },
+                const access_token = jwt.sign(
+                    { username: decoded.username, roles: decoded.roles },
                     process.env.ACCESS_TOKEN_SECRET,
                     {
                         expiresIn: "1s",
                     }
                 );
                 // send new access token
-                res.cookie("accesstoken", token);
+                res.cookie("accesstoken", access_token);
                 req.user = decoded;
                 next();
                 console.log("New access token created");
             } catch (error) {
-                return res.status(401).json({
+                return res.status(StatusCodes.BAD_REQUEST).json({
                     message: "Invalid token, refresh token expired",
                 });
             }
