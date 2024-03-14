040a4dc44acaf39d6dd8f663de6eecbc6905b027
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-03-14 11:58:59 +0300

    changed session middleware

--- server/middleware/sessionAuthorization.js
+++ server/middleware/sessionAuthorization.js
@@ -1,53 +1,14 @@
-const sessionAuthorization = (req, res, next) => {
-    console.log(req.cookies);
-    const refresh_token = req.cookies.refreshToken; // Updated cookie name
-    const access_token = req.cookies.accessToken; // Updated cookie name
+function sessionAuthorization(req, res, next) {
+    const authorized = req.session?.authorized;
 
-    console.log(access_token);
-
-    // check if there are no tokens
-    if (!access_token && !refresh_token) {
-        return res.json({
-            message: "Access denied. No tokens",
-        });
+    if (req.session && authorized) {
+        // Session is valid and authorized
+        console.log(req.session.user);
+        next(); // Proceed to the next middleware or route handler
+    } else {
+        // Session is invalid or unauthorized
+        res.status(401).send("You are not logged in");
     }
-
-
-    try {
-        // check if access token is valid
-        const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        if (refresh_token) {
-            // check if refresh token is valid
-            try {
-                const decoded = jwt.verify(
-                    refresh_token,
-                    process.env.REFRESH_TOKEN_SECRET
-                );
-
-                // create new access token
-                const newAccessToken = jwt.sign(
-                    { username: decoded.username, roles: decoded.roles },
-                    process.env.ACCESS_TOKEN_SECRET,
-                    {
-                        expiresIn: "15m", // Increased expiration time
-                    }
-                );
-
-                // send new access token
-                res.cookie("accessToken", newAccessToken); // Updated cookie name
-                req.user = decoded;
-                next();
-                console.log("New access token created");
-            } catch (error) {
-                return res.json({
-                    message: "Invalid token, refresh token expired",
-                });
-            }
-        }
-    }
-};
+}
 
 module.exports = { sessionAuthorization };
