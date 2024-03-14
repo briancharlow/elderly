0102ae44afaa5239a26d83c5a48cf137a73334e5
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-03-14 13:37:38 +0300

    changes the session access

--- server/middleware/sessionAuthorization.js
+++ server/middleware/sessionAuthorization.js
@@ -3,10 +3,9 @@ function sessionAuthorization(req, res, next) {
 
     if (req.session && authorized) {
         // Session is valid and authorized
-        console.log("this is the session")
-        // console.log(req.session)
+
         console.log(req.session.user);
-        console.log("above is the req.session.user")
+
         next(); // Proceed to the next middleware or route handler
     } else {
         // Session is invalid or unauthorized
