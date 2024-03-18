39f4b7a248094cca3661bf82b57a71849fd59d35
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-03-18 14:16:37 +0300

    remove unnecesary logs

--- client/src/Pages/Signup/caregiver.jsx
+++ client/src/Pages/Signup/caregiver.jsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Form, Button, Container, Row, Col, Alert } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import { useNavigate } from 'react-router-dom'
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import { useNavigate } from "react-router-dom";
 
 const CaregiverSignUpForm = ({ onSignUp }) => {
   const [phone, setPhone] = useState("");
@@ -12,17 +12,17 @@ const CaregiverSignUpForm = ({ onSignUp }) => {
   const [certificationId, setCertificationId] = useState("");
   const [showAlert, setShowAlert] = useState(false);
   const [alertMessage, setAlertMessage] = useState("");
-  
+
   const location = useLocation();
   const user = location.state.user;
 
-  console.log('this is the user:',user)
+  console.log("this is the user:", user);
 
   const navigate = useNavigate();
 
   const caregiver = { ...user, phone, description, password, certificationId };
 
-  console.log(caregiver)
+  console.log("this is the caregiver:", caregiver);
 
   const handleSubmit = async () => {
     try {
@@ -30,33 +30,40 @@ const CaregiverSignUpForm = ({ onSignUp }) => {
         "http://localhost:5000/createcaregiver",
         caregiver
       );
-    console.log(response);
-     const data = response.data;
-     console.log(data);
+      console.log(response);
+      const data = response.data;
+      console.log(data);
 
-     if(data.success){
-      alert("signup succesfull")
-      navigate("/login");
-     }
-     else{
-      alert("signup failed, check credentials and try again")
-     }
-
+      if (data.success) {
+        alert("signup succesfull");
+        navigate("/login");
+      } else {
+        alert("signup failed, check credentials and try again");
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
   return (
-    <Container className="mt-5" style={{ border: "1px solid #ccc", backgroundColor: "#f0f0f0", padding: "20px" }}>
+    <Container
+      className="mt-5"
+      style={{
+        border: "1px solid #ccc",
+        backgroundColor: "#f0f0f0",
+        padding: "20px",
+      }}
+    >
       <Row className="justify-content-md-center">
         <Col md={6}>
           <Form onSubmit={handleSubmit}>
             <h1 className="text-center mb-4">Welcome Caregiver</h1>
-            
+
             {showAlert && (
               <Alert
-                variant={alertMessage.includes("successful") ? "success" : "danger"}
+                variant={
+                  alertMessage.includes("successful") ? "success" : "danger"
+                }
                 onClose={() => setShowAlert(false)}
                 dismissible
               >
@@ -64,7 +71,11 @@ const CaregiverSignUpForm = ({ onSignUp }) => {
               </Alert>
             )}
 
-            <FloatingLabel controlId="formCertificationId" label="Certification ID" className="mb-3">
+            <FloatingLabel
+              controlId="formCertificationId"
+              label="Certification ID"
+              className="mb-3"
+            >
               <Form.Control
                 type="text"
                 placeholder="Enter Certification ID"
@@ -73,7 +84,11 @@ const CaregiverSignUpForm = ({ onSignUp }) => {
                 required
               />
             </FloatingLabel>
-            <FloatingLabel controlId="formPhoneNumber" label="Phone Number" className="mb-3">
+            <FloatingLabel
+              controlId="formPhoneNumber"
+              label="Phone Number"
+              className="mb-3"
+            >
               <Form.Control
                 type="text"
                 placeholder="Enter phone number"
@@ -82,8 +97,12 @@ const CaregiverSignUpForm = ({ onSignUp }) => {
                 required
               />
             </FloatingLabel>
-           <FloatingLabel controlId="formJobDescription" label="Job Description" className="mb-3">
-                <Form.Control
+            <FloatingLabel
+              controlId="formJobDescription"
+              label="Job Description"
+              className="mb-3"
+            >
+              <Form.Control
                 as="textarea"
                 placeholder="Enter job description"
                 value={description}
@@ -91,9 +110,13 @@ const CaregiverSignUpForm = ({ onSignUp }) => {
                 style={{ overflowY: "hidden" }}
                 required
               />
-          </FloatingLabel>
+            </FloatingLabel>
 
-            <FloatingLabel controlId="formPassword" label="Password" className="mb-3">
+            <FloatingLabel
+              controlId="formPassword"
+              label="Password"
+              className="mb-3"
+            >
               <Form.Control
                 type="password"
                 placeholder="Password"
