be5003dd19ded18e95ccdebaeac5b5a41cdec134
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-03-20 19:07:46 +0300

    created the route for retrieving routes

--- server/Routes/clientRoutes.js
+++ server/Routes/clientRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const clientRoute = express.Router();
 
-const { searchCaregiver, requestAppointment, rateCaregivers } = require('../Controllers/clientControllers');
+const { searchCaregiver, requestAppointment, rateCaregivers, getRatings } = require('../Controllers/clientControllers');
 
 const { sessionAuthorization } = require('../middleware/sessionAuthorization');
 
@@ -10,5 +10,6 @@ clientRoute.use(sessionAuthorization);
 clientRoute.get('/searchcaregiver/:searchTerm', searchCaregiver);
 clientRoute.post('/requestappointment', requestAppointment);
 clientRoute.post('/rate/:caregiverId', rateCaregivers);
+clientRoute.get('/ratings/:caregiverId', getRatings);
 
 module.exports = { clientRoute };
