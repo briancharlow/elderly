e5e7c949deb58adc9a689bb8d2ee4ef5e29aaf90
briancharlow <mwangangi.brian20@students.dkut.ac.ke>
2024-03-25 12:57:57 +0300

    imports the rating css

--- client/src/Components/Rating/RatingForm.jsx
+++ client/src/Components/Rating/RatingForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./RatingForm.css";
+import "./rating.css";
 
 const RatingForm = ({ caregiverId }) => {
   const [rating, setRating] = useState(0);
